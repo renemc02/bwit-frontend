0f66a12ff1190df4599de004db2dd13ab709d8e4
Rene Marreros <renemc02@gmail.com>
2026-08-12 10:42:26 -0500

    Actualizar hitos avance 001e

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: '/api',
   // ⚠️ Reemplaza con la URL real de tu App Service de Azure (sin barra final):
-  apiBaseUrl: 'http://localhost:63872',//'https://api-bwit-h5hkbjffbkdycdbr.canadacentral-01.azurewebsites.net',
+  apiBaseUrl: 'https://api-bwit-h5hkbjffbkdycdbr.canadacentral-01.azurewebsites.net',//'https://api-bwit-h5hkbjffbkdycdbr.canadacentral-01.azurewebsites.net',
   appVersion: '1.0.0',
 };
